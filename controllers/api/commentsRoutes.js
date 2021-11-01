7aaaa57c0a93a80b78d3b33ff2e1755807420f21
Will Gresham <willgresham34@gmail.com>
2021-11-01 18:50:15 -0400

    main handlebars, sign up and profile done

---
+++ controllers/api/commentsRoutes.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+const { Comments } = require("../../models/");
+const { withAuth } = require("../../utils/withAuth");
+
+router.post("/", withAuth, async (req, res) => {
+  try {
+    const newComments = await Comments.create(req.body);
+    res.json(newComments);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
