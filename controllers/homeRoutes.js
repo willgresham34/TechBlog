37eeaa39a2aca855aefbc0b033c52eccfc8308bd
Will Gresham <willgresham34@gmail.com>
2021-11-01 19:43:32 -0400

    login and signup pages show

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,3 +1,66 @@
 const router = require("express").Router();
 const { User, Post, Comments } = require("../models/");
 const { withAuth } = require("../utils/withAuth");
+
+router.get("/", async (req, res) => {
+  try {
+    if (req.session.loggedIn) {
+      res.redirect("/homepage");
+    }
+    res.render("login");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/sign-up", async (req, res) => {
+  try {
+    if (req.session.loggedIn) {
+      res.redirect("/homepage");
+    }
+    res.render("signUp", { logged_in: req.session.loggedIn });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/login", async (req, res) => {
+  try {
+    if (req.session.loggedIn) {
+      res.redirect("/homepage");
+      return;
+    }
+    res.render("login", { logged_in: req.session.loggedIn });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/homepage", withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [
+        User,
+        {
+          model: Comments,
+          include: User,
+        },
+      ],
+      order: [["created_at", "DESC"]],
+    });
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+    let randomIndex = Math.floor(Math.random() * quoteList.length);
+
+    console.log("Posts on Homepage: ", posts);
+
+    res.render("homepage", {
+      posts,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,7 +5,10 @@ const exphbs = require("express-handlebars");
 const helpers = require("./utils/helpers");
 const routes = require("./controllers");
 
-const sequelize = require("./config/config");
+const app = express();
+const PORT = process.env.PORT || 3002;
+
+const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sess = {
